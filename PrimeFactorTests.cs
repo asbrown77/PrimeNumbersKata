e0930b8f1a71ee94eebd77bb6165a6f6686fb28a
Alexander Brown <Alexander Brown>
2017-10-18 07:40:57 +0100

    Add project files.

---
+++ PrimeFactorTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace PrimeFactorKata
+{
+    [TestFixture]
+    public class PrimeFactorTests
+    {
+        [Test]
+        public void Generate_WhenZero_EmptyList()
+        {
+            var result = PrimeFactor.GeneratePrime(0);
+            Assert.That(result, Is.EqualTo(new int[] { }));
+        }
+    }
+
+    public static class PrimeFactor
+    {
+        public static List<int> GeneratePrime(int number)
+        {
+            return new List<int>();
+        }
+    }
+}
